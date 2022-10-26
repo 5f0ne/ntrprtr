948bc124e12131b56f7277d3f3f0f3e2bc4a6295
5f0 <96919731+5f0ne@users.noreply.github.com>
2022-10-26 17:20:25 +0200

    Version updated

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r") as r:
 
 setup(
     name="ntrprtr",            
-    version="2.0.3",
+    version="2.0.4",
     author="5f0",
     url="https://github.com/5f0ne/ntrprtr",
     description="Interpret bytes through different customizable actions",
